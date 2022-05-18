eb0375de4905c8a9023a8405ce916cd707362a6c
Mauro Candotti <cmduilio@gmail.com>
2022-05-18 15:30:20 -0300

    finished request migration

--- src/db/migrations/20220517231403-CreateRequest.js
+++ src/db/migrations/20220517231403-CreateRequest.js
@@ -2,20 +2,56 @@
 
 module.exports = {
   async up (queryInterface, Sequelize) {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
+    
+    await queryInterface.createTable('solicitudes', { 
+      
+      id: {
+        type: Sequelize.DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+
+      idOwner: {
+        type: Sequelize.DataTypes.INTEGER,
+        allowNull: false,
+      },
+
+      idMascot: {
+        type: Sequelize.DataTypes.INTEGER,
+        allowNull: false,
+      },
+
+      idRequester: {
+        type: Sequelize.DataTypes.INTEGER,
+        allowNull: false,
+      },
+
+      status: {
+        type: Sequelize.DataTypes.ENUM('open', 'rejected', 'accepted'),
+        defaultValue: 'open',
+      },
+
+      createdAt: {
+        type: Sequelize.DataTypes.DATE,
+        defaultValue: Sequelize.DataTypes.NOW
+      },
+
+      updateAt: {
+        type: Sequelize.DataTypes.DATE,
+        defaultValue: Sequelize.DataTypes.NOW
+      },
+
+      deletedAt: {
+        type: Sequelize.DataTypes.DATE,
+        defaultValue: Sequelize.DataTypes.NOW
+      }
+    });
+
   },
 
   async down (queryInterface, Sequelize) {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
+  
+    await queryInterface.dropTable('solicitudes');
+     
   }
 };
