898364e8c7b8120e0626fe29d5bed48714c40b54
facusafe <facundosafe10@gmail.com>
2022-05-24 19:53:12 -0300

    seeder de users

--- src/db/seeders/20220524223841-users.js
+++ src/db/seeders/20220524223841-users.js
@@ -1,24 +1,45 @@
 'use strict';
 
+const {randBetweenDate, randStreetAddress, randEmail, randUserName, randFirstName, randLastName, randPassword, randNumber, randText} = require('@ngneat/falso');
+
 module.exports = {
   async up (queryInterface, Sequelize) {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
+
+    let usuarios = []
+
+      for(var i = 0; i < 1000; i++){
+
+        usuarios.push({
+          userName : randUserName(),
+    
+          password : randPassword(),
+      
+          name : randFirstName(),
+      
+          lastName : randLastName(),
+      
+          email : randEmail(),
+      
+          address : randStreetAddress(),
+      
+          phoneNumber : randNumber({min: 10000000, max: 90000000}),
+      
+          requise : randText(),
+    
+          createdAt: randBetweenDate({from: new Date('01/01/2012'), to: new Date('01/01/2015')}),
+    
+          updatedAt: randBetweenDate({from: new Date('10/07/2015'), to: new Date('01/01/2021')})
+        });
+
+      }
+ 
+    await queryInterface.bulkInsert('users', usuarios, {});
+   
   },
 
   async down (queryInterface, Sequelize) {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
+    
+    await queryInterface.bulkDelete('users', null, {});
+
   }
 };
