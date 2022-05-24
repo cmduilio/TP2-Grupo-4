46acda32c525498410d8acf18e39290f0f0ef4c6
facusafe <facundosafe10@gmail.com>
2022-05-24 13:44:25 -0300

    generacion de la fake data para la db

--- src/db/seeders/20220524160520-mascots.js
+++ src/db/seeders/20220524160520-mascots.js
@@ -1,24 +1,34 @@
 'use strict';
 
+const {randAnimalType, randFirstName, randBoolean, randNumber, randText} = require('@ngneat/falso');
+
 module.exports = {
   async up (queryInterface, Sequelize) {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
+    let mascots = [];
+
+    for(let i = 0; i < 100; i++){
+      mascots.push({
+        especie: "animal",
+        raza: randAnimalType(),
+        nombre: randFirstName(),
+        tamanio: "mediano",
+        edad: randNumber({min: 0, max: 10}),
+        buscaDuenio: randBoolean(),
+        estaVacunado: randBoolean(),
+        estaCastrado: randBoolean(),
+        comentario: randText(),
+        createdAt: new Date,
+        updatedAt: new Date,
+      });
+    }
+
+    await queryInterface.bulkInsert('mascots', mascots, {});
+
   },
 
   async down (queryInterface, Sequelize) {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
+  
+    await queryInterface.bulkDelete('mascots', null, {});
+
   }
 };
