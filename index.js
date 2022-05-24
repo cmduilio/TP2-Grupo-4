8eca340a08555b110e87ac1e437799686517d056
facusafe <facundosafe10@gmail.com>
2022-05-24 13:04:49 -0300

    implementacion busqueda de todas las mascotas

--- index.js
+++ index.js
@@ -6,8 +6,11 @@ app.get('/', function(req, res) {
     res.send('hello');
 });
 
-app.get('/mascots', function(req, res) {
-    res.send([]);
+app.get('/mascots', async function(req, res) {
+
+    let data = await Mascot.findAll();
+
+    res.send(data);
 })
 
 app.listen(8001);
