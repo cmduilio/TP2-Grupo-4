1c7af8d8e3c78a5fd11c3755e0d10361a71fea20
facusafe <facundosafe10@gmail.com>
2022-05-24 21:15:16 -0300

    mejoro el seeder de mascots

--- src/db/seeders/20220524160520-mascots.js
+++ src/db/seeders/20220524160520-mascots.js
@@ -1,17 +1,49 @@
 'use strict';
 
-const {randBetweenDate, randAnimalType, randFirstName, randBoolean, randNumber, randText} = require('@ngneat/falso');
+const {randBetweenDate, randBird, randDog, randCat, randFirstName, randBoolean, randNumber, randText} = require('@ngneat/falso');
 
 module.exports = {
   async up (queryInterface, Sequelize) {
     let mascots = [];
 
-    for(let i = 0; i < 100; i++){
+    for(let i = 0; i < 30; i++){
       mascots.push({
-        species: "animal",
-        race: randAnimalType(),
+        animal: "perro",
+        race: randDog(),
+        name: randFirstName(),
+        size: "grande",
+        age: randNumber({min: 0, max: 10}),
+        looksForOwner: randBoolean(),
+        isVaccinated: randBoolean(),
+        isCastrated: randBoolean(),
+        comment: randText(),
+        createdAt: randBetweenDate({from: new Date('01/01/2012'), to: new Date('01/01/2015')}),
+        updatedAt: randBetweenDate({from: new Date('10/07/2015'), to: new Date('01/01/2021')})
+      });
+    }
+
+    for(let i = 0; i < 30; i++){
+      mascots.push({
+        animal: "gato",
+        race: randCat(),
         name: randFirstName(),
         size: "mediano",
+        age: randNumber({min: 0, max: 10}),
+        looksForOwner: randBoolean(),
+        isVaccinated: randBoolean(),
+        isCastrated: randBoolean(),
+        comment: randText(),
+        createdAt: randBetweenDate({from: new Date('01/01/2012'), to: new Date('01/01/2015')}),
+        updatedAt: randBetweenDate({from: new Date('10/07/2015'), to: new Date('01/01/2021')})
+      });
+    }
+
+    for(let i = 0; i < 10; i++){
+      mascots.push({
+        animal: "otro",
+        race: randBird(),
+        name: randFirstName(),
+        size: "chico",
         age: randNumber({min: 0, max: 10}),
         looksForOwner: randBoolean(),
         isVaccinated: randBoolean(),
