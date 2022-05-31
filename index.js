95f33084e5ba3bad4804f8e149366f89c5fedc0e
LogicalTrue <59065051+LogicalTrue@users.noreply.github.com>
2022-05-31 08:23:20 -0300

    Agrego en changeemail un send

--- index.js
+++ index.js
@@ -68,6 +68,10 @@ app.get('/users/:id', async function(req, res){
            where : {id : req.params.id}   
           
           })
+
+          let data = await User.findByPk(req.params.id)
+
+          res.send(data);
       })
 
     //actualizar Direccion
