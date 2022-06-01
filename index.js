961c0fc4f37acbc0025ab1deae395fa4ac358ca3
facusafe <facundosafe10@gmail.com>
2022-05-31 22:17:03 -0300

    index correction

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const express = require('express');
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended: true}))
-const {Pet} = require('./src/db/models');
 
 const {Request, User, Pet} = require('./src/db/models');
 
