58b761a8e6a03f7bbe863630618ac3251a5e489d
Mauro Candotti <cmduilio@gmail.com>
2022-06-05 17:12:31 -0300

    added some validations and error responses

--- src/controllers/RequestController.js
+++ src/controllers/RequestController.js
@@ -4,25 +4,35 @@ var router = express.Router()
 const { Request } = require('../db/models');
 
 router.get('/requests', async function (req, res) {
-
-    console.log("alerta");
-    res.status(422).json({ message: 'pep' })
-
     let data = await Request.findAll();
     res.send(data);
 })
 
 router.get('/requests/:id', async function (req, res) {
     let data = await Request.findByPk(req.params.id);
-
-    res.send(data);
+    if (!data || data === ''){
+        res.status(422).json(
+            {
+                message: `error getting request with id ${req.params.id}`
+            }
+        )
+    }
+    res.status(200).send(data);
 })
 
 router.post('/requests', async function (req, res) {
-    console.log(req.body)
-    await Request.create(req.body)
-    .then(data => {res.status(201).json({})})
-    .catch(err => {res.status(422).json({})});
+    const request = req.body;
+    const errorResponse = {
+        message : 'Error creating request'
+    };
+
+    await Request.create(request)
+    .then(data => {
+        res.status(201).sent(data)
+    })
+    .catch(err => {
+        res.status(422).json(errorResponse)
+    });
 })
 
 module.exports = router;
