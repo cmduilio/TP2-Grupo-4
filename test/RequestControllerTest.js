030a0ffe2974d44e87d60d417e3925249c9090cb
Mauro Candotti <cmduilio@gmail.com>
2022-06-05 17:13:03 -0300

    added tests for good and bad responses of requests

--- test/RequestControllerTest.js
+++ test/RequestControllerTest.js
@@ -6,18 +6,67 @@ chai.use(chaiFetch);
 const { assert } = chai;
 
 describe('Request', () => {
-    it('return something', (done) =>{
+    it('returns all requests', (done) =>{
         axios({
             method: 'get',
             url: 'http://localhost:6001/requests',
         })
         .then(data => {
-            console.log("bien");
-            console.log(data.body);
+            assert.isTrue(data.data.length > 0)
             done();
+        });
+    });
+
+    it('returns request with id', (done) =>{
+        axios({
+            method: 'get',
+            url: 'http://localhost:6001/requests/30',
         })
-        .catch(err => {
-            assert.equal(err.response.data.message, 'pep')
+        .then(data => {
+            assert.equal(data.data.id, 30)
+            done();
+        });
+    });
+
+    it('return error with unknown id', (done) =>{
+        axios({
+            method: 'get',
+            url: 'http://localhost:6001/requests/4000',
+        })
+        .catch(error => {
+            assert.equal(error.response.data.message, `error getting request with id 4000`)
+            done();
+        });
+    });
+
+    it('return error creating request', (done) =>{
+        axios({
+            method: 'post',
+            url: 'http://localhost:6001/requests',
+            data: {
+                id : 30
+            }
+        })
+        .catch(error => {
+            assert.equal(error.response.data.message, `Error creating request`)
+            done();
+        });
+    });
+
+    //TODO DUILIO: por alguna razon esto devuelve error pero crea el objeto en base
+    it.skip('return created request', (done) =>{
+        axios({
+            method: 'post',
+            url: 'http://localhost:6001/requests',
+            data: {
+                idOwner: 1,
+                idMascot: 1,
+                idRequester: 2,
+                status: "open"
+            }
+        })
+        .then(data => {
+            assert.isTrue(data.id)
             done();
         });
     });
