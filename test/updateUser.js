9c2bed45878d6d4aafd7185f9851ec224267877b
LogicalTrue <59065051+LogicalTrue@users.noreply.github.com>
2022-06-06 17:20:18 -0300

    Agrego test si se cambia el id, devuelve error

--- test/updateUser.js
+++ test/updateUser.js
@@ -57,6 +57,30 @@ describe('Update user', () => {
     });
 
 
+    it('returns 400 if update the ID attribute is fail', (done)=> {
+        const userid = 3;    
+        const user = {
+                id: -userid,
+        };
 
+        axios({
+            method: "patch",
+            ///!!! Pendiente de cambio: "/users/:id"
+            url: `http://localhost:6001/users/${userid}/updateuser`,
+            data: user,
+        }).then(response => {
+            assert.equal(response.status, 400);
+            done();
+        }).catch(err => {
+            if(Object.keys(err).includes("response"))
+            {
+                assert.equal(err.response.status, 400); 
+                done();
+            }
+            else done(err);
+        }).catch(err => {
+            done(err);
+        })
+    });
 
 })
