4027d956028003435f55140df441310f030a2ad9
facusafe <facundosafe10@gmail.com>
2022-06-06 12:59:37 -0300

    agrego caso positivo/error para actualiza mascota

---
+++ test/updatePet.js
@@ -0,0 +1,39 @@
+const chai = require('chai');
+const chaiFetch = require('chai-fetch');
+chai.use(chaiFetch);
+const axios = require('axios');
+const { assert } = require('chai');
+
+describe('Add Pet', () => {
+
+    it('returns 201 if pet is updated', (done)=> {
+        
+        axios({
+            method: "patch",
+            url: 'http://localhost:8001/pets/3/update',
+            data: {
+                name: 'Rocco',
+            }
+        }).then(response => {
+            // testeo
+            assert.equal(response.status, 201);
+            done();
+        }).catch(err => {
+            done();
+        })
+    });
+
+    it('returns 422 if pet animal type is modified', (done)=> {
+        
+        axios({
+            method: "patch",
+            url: 'http://localhost:8001/pets/3/update',
+            data: {
+                animal: 'gato',
+            }
+        }).catch(err => {
+            assert.equal(err.response.status, 422);
+            done();
+        })
+    });
+})
