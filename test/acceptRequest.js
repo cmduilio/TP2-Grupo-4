51d1e3d4d5f334693cbf7dae9a3f81ab75ae3e93
LogicalTrue <59065051+LogicalTrue@users.noreply.github.com>
2022-06-07 17:57:38 -0300

    Agrego test de error enviando una id no existente

--- test/acceptRequest.js
+++ test/acceptRequest.js
@@ -129,6 +129,32 @@ it('returns 400 if request is rejected by a bad body', (done)=> {
     })
 });
 
+it('returns 400 if request isn\'t exist', (done)=> {
+           
+    const requestId = -9;   // deberia ser dinamico
+    const body ={ requestKey: 690+388 };  
+
+     axios({
+         method: "put",
+         url: `http://localhost:6001/accept-request/${requestId}`,
+         data: body
+         
+     }).then(response => {
+         assert.equal(response.status, 400);
+         done();
+     }).catch(err => {
+        if(Object.keys(err).includes("response"))
+        {
+            assert.equal(err.response.status, 400); 
+            done();
+        }
+        else done(err);
+    }).catch(err => { 
+        done(err);
+    })
+});
+
+
 
 
 })
