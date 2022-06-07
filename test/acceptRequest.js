843ed7d09c22f3d33e121c0110e6776aab8dee2b
LogicalTrue <59065051+LogicalTrue@users.noreply.github.com>
2022-06-07 17:55:43 -0300

    Agrego test de error enviando requestKey erronea

--- test/acceptRequest.js
+++ test/acceptRequest.js
@@ -52,6 +52,31 @@ describe('Accept Request', () => {
     })
 });
 
+it('returns 400 if request is rejected by bad Key', (done)=> {
+           
+    const requestId = 9;   // deberia ser dinamico
+    const body ={ requestKey: 690-388 };  
+
+     axios({
+         method: "put",
+         url: `http://localhost:6001/accept-request/${requestId}`,
+         data: body
+         
+     }).then(response => {
+         assert.equal(response.status, 400);
+         done();
+     }).catch(err => {
+        if(Object.keys(err).includes("response"))
+        {
+            assert.equal(err.response.status, 400); 
+            done();
+        }
+        else done(err);
+    }).catch(err => { 
+        done(err);
+    })
+});
+
 
 
 })
