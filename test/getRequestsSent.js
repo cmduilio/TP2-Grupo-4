d2043e5a350666879b574aaf26835fae221ef0e0
facusafe <facundosafe10@gmail.com>
2022-06-10 20:23:20 -0300

    test/get para obtener requests hechas por usuario

---
+++ test/getRequestsSent.js
@@ -0,0 +1,22 @@
+const chai = require('chai');
+const chaiFetch = require('chai-fetch');
+chai.use(chaiFetch);
+const axios = require('axios');
+const { assert } = require('chai');
+
+describe('Get requests sent', () => {
+
+    it('returns 200 when showing requests made by user', (done)=> {
+        
+        axios({
+            method: "get",
+            url: 'http://localhost:8001/requests-sent',
+        }).then(response => {
+            // testeo
+            assert.equal(response.status, 200);
+            done();
+        }).catch(err => {
+            done();
+        })
+    });
+})
