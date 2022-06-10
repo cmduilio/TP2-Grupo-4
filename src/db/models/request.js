8ebaca69f5fa32da19ac39a967991a36133336f5
LogicalTrue <59065051+LogicalTrue@users.noreply.github.com>
2022-06-10 17:59:33 -0300

    Corrijo request

--- src/db/models/request.js
+++ src/db/models/request.js
@@ -11,7 +11,6 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
-      Request.belongsTo(models.Pet, {foreignKey : "idMascot"});
     }
   }
   Request.init({ 
