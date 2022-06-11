4132afcf8d74e30d7c4dd1419353c2b3137c29ff
facusafe <facundosafe10@gmail.com>
2022-06-11 13:34:30 -0300

    test

---
+++ test/receivedRequests.js
@@ -0,0 +1,22 @@
+const chai = require('chai');
+const chaiFetch = require('chai-fetch');
+chai.use(chaiFetch);
+const axios = require('axios');
+const { assert } = require('chai');
+
+describe('Get requests sent', () => {
+
+    it('returns 200 when showing users received requests', (done)=> {
+        
+        axios({
+            method: "get",
+            url: 'http://localhost:8001/received-requests',
+        }).then(response => {
+            // testeo
+            assert.equal(response.status, 200);
+            done();
+        }).catch(err => {
+            done();
+        })
+    });
+})
