7ac8ad04f42d8353103bfd8f136e2840c2b7e365
facusafe <facundosafe10@gmail.com>
2022-06-13 11:22:15 -0300

    quito requests hardcodeadas del seeder

--- src/db/seeders/20220518210532-requests.js
+++ src/db/seeders/20220518210532-requests.js
@@ -17,45 +17,6 @@ module.exports = {
       })
     }
 
-    //request for reject-request testing
-    requests.push({
-        id: 1000,  
-        idOwner: 20,
-        idMascot: randNumber({min: 1, max: 50}),
-        idRequester: randNumber({min: 1, max: 50}),
-        status: 'open',
-        createdAt: new Date,
-        updatedAt: new Date,
-    },
-    {
-      id: 1001,
-      idOwner: 1,
-      idMascot: randNumber({min: 1, max: 50}),
-      idRequester: randNumber({min: 1, max: 50}),
-      status: 'open',
-      createdAt: new Date,
-      updatedAt: new Date,
-    },
-    {
-      id: 1002,
-      idOwner: 20,
-      idMascot: randNumber({min: 1, max: 50}),
-      idRequester: randNumber({min: 1, max: 50}),
-      status: 'rejected',
-      createdAt: new Date,
-      updatedAt: new Date,
-    },
-    {
-      id: 1003,
-      idOwner: 20,
-      idMascot: randNumber({min: 1, max: 50}),
-      idRequester: randNumber({min: 1, max: 50}),
-      status: 'accepted',
-      createdAt: new Date,
-      updatedAt: new Date,
-    }
-    )
-
     await queryInterface.bulkInsert('requests', requests, {});
   },
 
