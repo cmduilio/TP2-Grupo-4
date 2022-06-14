61160d2e87ea79d4abe25d5824d91ba7c1cdf05a
LogicalTrue <59065051+LogicalTrue@users.noreply.github.com>
2022-06-14 14:55:10 -0300

    Asigno test donde la id es invalida

--- test/requestByAnimal.js
+++ test/requestByAnimal.js
@@ -7,7 +7,19 @@ const { randNumber } = require('@ngneat/falso');
 
 describe('Request by animal', () => {
 
+    it('returns 400 when this id is invalid', (done)=> {
+        
+        const idAnimal = randNumber({min: 1, max: 50});
 
+        axios({
+            method: "get",
+            url: `http://localhost:8001/pets/totalrequestsbypet/${-idAnimal}`,
+        }).then(response => {
+            assert.equal(response.status, 400);
+        }).catch(err => {
+            done();
+        })
+    });
 
 
 })
