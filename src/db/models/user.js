6b3edc7e8362905454d8b6ae7824c979e8275a8e
facusafe <facundosafe10@gmail.com>
2022-06-14 19:24:48 -0300

    agrego asociacion

--- src/db/models/user.js
+++ src/db/models/user.js
@@ -11,6 +11,7 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       User.hasMany(models.Pet, {foreignKey : "userId"});
+      User.hasMany(models.Request, {foreignKey : "idRequester"});
     }
   }
   User.init({
