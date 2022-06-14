a379d54a4bfbeb43e2c353f5ce88e8e9825f4e49
Mauro Candotti <cmduilio@gmail.com>
2022-06-14 17:42:47 -0300

    association to pet

--- src/db/models/request.js
+++ src/db/models/request.js
@@ -10,8 +10,8 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      // define association here
-      Request.belongsTo(models.Pet, {foreignKey : "idMascot"});
+      // define association here  
+    Request.belongsTo(models.Pet, {foreignKey : "idMascot"});
     }
   }
   Request.init({ 
