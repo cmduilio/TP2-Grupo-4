128dc88f4b230985f2ecc8ab35dd4c01d4fd4b28
LogicalTrue <59065051+LogicalTrue@users.noreply.github.com>
2022-06-14 14:55:59 -0300

    Asigno test donde se procesa correctamente la solicitud

--- test/requestByAnimal.js
+++ test/requestByAnimal.js
@@ -22,4 +22,19 @@ describe('Request by animal', () => {
     });
 
 
+    it('returns 200 when showing total count of requests related to animal ', (done)=> {
+        
+        const idAnimal = randNumber({min: 1, max: 50});   
+
+        axios({
+            method: "get",
+            url: `http://localhost:8001/pets/totalrequestsbypet/${idAnimal}`,
+        }).then(response => {
+            assert.equal(response.status, 200);
+        }).catch(err => {
+            done();
+        })
+    });
+
+
 })
