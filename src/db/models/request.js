c948cd214e3cadb9099b1a063ce6d89ed8a11e0e
facusafe <facundosafe10@gmail.com>
2022-06-14 19:24:36 -0300

    agrego asociacion

--- src/db/models/request.js
+++ src/db/models/request.js
@@ -10,7 +10,8 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      // define association here
+      Request.belongsTo(models.User, {foreignKey: "idRequester"});
+      /* Request.belongsTo(models.User, {foreignKey: "idOwner"}); */
     }
   }
   Request.init({ 
