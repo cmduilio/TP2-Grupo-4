e9a2daff386d0b95bcfece6404c8a6e8b8ae850e
LogicalTrue <59065051+LogicalTrue@users.noreply.github.com>
2022-06-14 14:53:34 -0300

    Creo archivo test de solicitudes por animal

---
+++ test/requestByAnimal.js
@@ -0,0 +1,13 @@
+const chai = require('chai');
+const chaiFetch = require('chai-fetch');
+chai.use(chaiFetch);
+const axios = require('axios');
+const { assert } = require('chai');
+const { randNumber } = require('@ngneat/falso');
+
+describe('Request by animal', () => {
+
+
+
+
+})
